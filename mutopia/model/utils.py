55ebf07d67d0c1f5f004af41f2afaf5d81778735
AllenWLynch <allen.lynch1@outlook.com>
2024-11-04 18:14:57 -0500

    added model/utils.py - just some nice functions to have in the namespace

---
+++ mutopia/model/utils.py
@@ -0,0 +1,37 @@
+import xarray as xr
+from .corpus_state import CorpusState as CS
+from joblib import Parallel
+from contextlib import contextmanager
+
+@contextmanager
+def ParContext(n_jobs, verbose=0):
+    yield Parallel(
+        n_jobs=n_jobs, 
+        backend='threading', 
+        return_as='generator', 
+        verbose=verbose,
+        pre_dispatch='n_jobs',
+    )
+
+
+def using_exposures_from(*corpuses):
+    
+    corpus_dict = {CS.get_name(corpus) : corpus for corpus in corpuses}
+    
+    return lambda corpus, sample_name : \
+        CS.fetch_topic_compositions(
+            corpus_dict[CS.get_name(corpus)],
+            sample_name
+        )
+
+
+def split_by_chrom(
+    corpus,
+    test_chroms = ('chr1',),
+):
+    corpus=corpus.load()
+    train_mask = ~(corpus.regions.chrom.data.isin(test_chroms))
+    train = corpus.isel(locus=train_mask)
+    test = corpus.isel(locus=~train_mask)
+
+    return train, test
