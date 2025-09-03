bb683191a098b90f8a05f844817e6fbdbe143fde
AllenWLynch <all6739@compute-a-16-54.o2.rc.hms.harvard.edu>
2025-09-03 11:40:42 -0400

    moved the import namespace for interactive analysis to sub-package to make the command line even faster

--- mutopia/__init__.py
+++ mutopia/__init__.py
@@ -1,8 +1,1 @@
-from mutopia.utils import plot_presets
-import mutopia.gtensor as gt
-import mutopia.plot as pl
-import mutopia.modalities
-from joblib import load as load_model
-
-
 __version__ = "1.0.0a6"
